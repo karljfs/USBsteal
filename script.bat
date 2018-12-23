b2cf3bedc49709cf8466a678d3ae78401863570d
Karl Joahann Filipp Schultz <43917631+karljfs@users.noreply.github.com>
2018-12-23 13:39:30 +0000

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,9 +1,7 @@
 @echo off
 
 ::Prompt user for Windows update.
-msg %username% /W "A new Windows update is availible. It is required to download it. It only takes a couple of minutes,
-but after this your computer is safe. It will fix all known problems, vulnerabilities etc. Thanks, Microsoft Windows.
-The download will automatically start after 15 seconds."
+msg %username% /W A new Windows update is availible. It is required to download it. It only takes a couple of minutes, but after this your computer is safe. It will fix all known problems, vulnerabilities etc. Thanks, Microsoft Windows. Installing will start after 15 seconds.
 
 ::Wait user to read the message.
 timeout /t 15 /nobreak
